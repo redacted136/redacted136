3713fa51220c80cbad54219ef24dd19d01a640e2
Weilin <dengweilin12989@gmail.com>
2025-11-15 21:52:30 +0800

    Finish ybc c++ codes

--- redacted136-home/C++/question2.cpp
+++ redacted136-home/C++/question2.cpp
@@ -2,7 +2,24 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World" << endl;
-    
+    int L;
+    int sum;
+    int counter;
+    int divisor;
+    cin >> L;
+    while(sum<L){
+        for(int i=2; i>0; i++){
+            if (i/divisor==0)
+            {
+                //没思路了
+                //找不到如何检测是否质数的算法
+                cout << i << endl;
+                break;
+            }
+            
+        }
+        counter++;
+        sum+=counter;
+    }
     return 0;
 }
---
+++ redacted136-home/C++/question3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    int loop;
+    int result;
+    vector<int> max_odd;
+    vector<int> numbers;
+    cin >> n;
+    for(int i=0; i<n; i++){
+        cin >> loop;
+        numbers.push_back(loop);
+    }
+    
+    for(int i=0; i<n;i++){
+        for(int j=i+1; j<n;j++){
+            result += numbers[i];
+            if(result%2!=0){
+                max_odd.push_back(result);
+                 
+            }
+        }
+        
+    }
+    for(int num : max_odd){
+        cout << num << endl;
+    }
+    return 0;
+}
